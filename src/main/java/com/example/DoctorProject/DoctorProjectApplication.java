27583d18008cd6e056d3117a983b5a2a74fe59ed
ReetishaDas <doreshinreeti@gmail.com>
2022-07-14 10:27:04 +0530

    seond commit

--- src/main/java/com/example/DoctorProject/DoctorProjectApplication.java
+++ src/main/java/com/example/DoctorProject/DoctorProjectApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DoctorProjectApplication {
 
 	public static void main(String[] args) {
-		System.out.println("CONNECTED");
+		System.out.println("*****************CONNECTED*************");
 		SpringApplication.run(DoctorProjectApplication.class, args);
 	}
 
